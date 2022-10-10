58292da6151aa25463cb019f940432c84f7d67d6
DinoSturlan <dsturlan@unipu.hr>
2022-10-11 01:39:41 +0200

    Rijesen zadatak 4

---
+++ Zadaca_1/Zadaca_1_4.js
@@ -0,0 +1,20 @@
+function pretvorba(a){
+    var b = parseInt(a / 60);
+    var c = parseInt(a % 60);
+
+    console.log("To je", b, "sati i", c, "minuta");
+}
+
+
+var x = 121;
+console.log("broj:", x);
+pretvorba(x);
+
+var y = 344;
+console.log("broj:", y);
+pretvorba(y);
+
+var z = 15;
+console.log("broj:", z);
+pretvorba(z);
+
